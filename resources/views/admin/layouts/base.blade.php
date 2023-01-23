bb9eb6094e2197cae8b2a1acef356f6ce8226426
LucaBonifazio <luca.bonifazio13@gmail.com>
2023-01-23 12:10:28 +0100

    little style change

--- resources/views/admin/layouts/base.blade.php
+++ resources/views/admin/layouts/base.blade.php
@@ -9,43 +9,42 @@
         <script src="{{ asset('js/app.js') }}"></script>
 
         <style>
+            a {
+            text-decoration: none;
+            }
 
-a {
-    text-decoration: none;
-}
+            .bg_container {
+            background-color: rgb(29, 29, 29);
+            }
 
-.bg_container {
-    background-color: rgb(29, 29, 29);
-  }
+            .cards_box {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex-wrap: wrap;
+            padding: 2em 0;
+            position: relative;
+            }
 
-  .cards_box {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-wrap: wrap;
-    padding: 2em 0;
-    position: relative;
-  }
-  .card {
-    flex: 0 0 calc(100% / 6);
-  }
+            .card {
+            flex: 0 0 16.6666666667%;
+            }
 
-  .card {
-    text-align: center;
-    padding: .1rem;
-    .contents {
-      padding: 1em;
-      height: 100%;
-      h2 {
-        color: white;
-        font-size: 1.2rem;
-      }
-      &:hover {
-        transform: scale(1.1);
-      }
-    }
-  }
-
+            .card {
+            text-align: center;
+            padding: 0.1rem;
+            }
+            .card .contents {
+            padding: 1em;
+            height: 100%;
+            }
+            .card .contents h2, .card .contents .h2 {
+            color: white;
+            font-size: 1.2rem;
+            }
+            .card .contents:hover {
+            transform: scale(1.1);
+            }
         </style>
     </head>
     <body>
